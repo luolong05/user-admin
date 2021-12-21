2bc13febb87057a9ebd2fb305587fc45244a960e
luolong05 <luolong05@163.com>
2021-12-21 17:14:34 +0800

    add function test

--- src/components/modules/userForm/userForm.test.tsx
+++ src/components/modules/userForm/userForm.test.tsx
@@ -1,15 +1,47 @@
 import React from "react";
-import {render, queryByAttribute} from "@testing-library/react";
+import {render, queryByAttribute, fireEvent, screen} from "@testing-library/react";
 import UserForm from "./index";
 
 describe("Test the form element of the userForm component", () => {
-  it('userForm must have a userName input and a commit button', () => {
-    const { container, getByText } = render(<UserForm />);
-
+  it('userForm must have a userName input', () => {
+    const { container } = render(<UserForm />);
     const userNameEl = queryByAttribute("name", container, "userName");
-    const commitBtn = getByText('Commit');
 
     expect(userNameEl).toBeInTheDocument();
+  });
+
+  it('userForm must have a commit button', () => {
+    const { getByText } = render(<UserForm />);
+    const commitBtn = getByText('Commit');
+
     expect(commitBtn).toBeInTheDocument();
-  })
-})
+  });
+});
+
+describe("Test the form function", () => {
+  it('userForm must have a correct userName value', () => {
+    const { container } = render(<UserForm />);
+    const userNameEl = queryByAttribute("name", container, "userName");
+
+    expect(userNameEl).toBeInTheDocument();
+  });
+
+  it('userForm must have a commit button', () => {
+    const { container, getByText } = render(<UserForm />);
+    const userNameInput = queryByAttribute("name", container, "userName");
+    const commitBtn = getByText('Commit');
+
+    expect(commitBtn).toBeDisabled();
+
+    if (userNameInput) {
+      fireEvent.input(userNameInput, { target: { value: '12345678901234567890123' } });
+    }
+
+    expect(commitBtn).toBeEnabled();
+    expect(userNameInput?.getAttribute("value")).toEqual('12345678901234567890');
+
+    fireEvent.click(commitBtn);
+
+    expect(screen.getByText('The information you entered is: 12345678901234567890')).toBeInTheDocument();
+  });
+});
