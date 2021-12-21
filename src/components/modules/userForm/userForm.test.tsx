918f63f25d954f7eeb984b030b9e158d933f79ea
luolong05 <luolong05@163.com>
2021-12-21 17:21:59 +0800

    update user form test; split test unit

--- src/components/modules/userForm/userForm.test.tsx
+++ src/components/modules/userForm/userForm.test.tsx
@@ -19,29 +19,46 @@ describe("Test the form element of the userForm component", () => {
 });
 
 describe("Test the form function", () => {
-  it('userForm must have a correct userName value', () => {
-    const { container } = render(<UserForm />);
-    const userNameEl = queryByAttribute("name", container, "userName");
-
-    expect(userNameEl).toBeInTheDocument();
-  });
-
-  it('userForm must have a commit button', () => {
-    const { container, getByText } = render(<UserForm />);
-    const userNameInput = queryByAttribute("name", container, "userName");
+  it('before input, the commit button is disabled', () => {
+    const { getByText } = render(<UserForm />);
     const commitBtn = getByText('Commit');
 
     expect(commitBtn).toBeDisabled();
+  });
+
+  it('after input, the commit button is enabled', () => {
+    const { container, getByText } = render(<UserForm />);
+    const userNameInput = queryByAttribute("name", container, "userName");
+    const commitBtn = getByText('Commit');
 
     if (userNameInput) {
       fireEvent.input(userNameInput, { target: { value: '12345678901234567890123' } });
     }
 
     expect(commitBtn).toBeEnabled();
+  });
+
+  it('The user name input value\'s max length is 20 ', () => {
+    const { container } = render(<UserForm />);
+    const userNameInput = queryByAttribute("name", container, "userName");
+
+    if (userNameInput) {
+      fireEvent.input(userNameInput, { target: { value: '12345678901234567890123' } });
+    }
+
     expect(userNameInput?.getAttribute("value")).toEqual('12345678901234567890');
+  });
+
+  it('after click commit button, show input result', () => {
+    const { container, getByText } = render(<UserForm />);
+    const userNameInput = queryByAttribute("name", container, "userName");
+    const commitBtn = getByText('Commit');
+
+    if (userNameInput) {
+      fireEvent.input(userNameInput, { target: { value: '12345678901234567890123' } });
+    }
 
     fireEvent.click(commitBtn);
-
-    expect(screen.getByText('The information you entered is: 12345678901234567890')).toBeInTheDocument();
+    expect(getByText('The information you entered is: 12345678901234567890')).toBeInTheDocument();
   });
 });
