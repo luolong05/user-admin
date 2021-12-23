c6443c4a2685c19309b58ef91705b2035e4476a9
luolong05 <luolong05@163.com>
2021-12-23 14:28:58 +0800

    add new theme

--- src/styles/global/theme.ts
+++ src/styles/global/theme.ts
@@ -1,9 +1,12 @@
 export default {
   colorDefault: "#d9d9d9",
+  colorDefaultActive: "#fff",
   colorPrimary: "#246ab9",
+  colorPrimaryActive: "#3284e1",
   colorWarning: "#FF9A2A",
   colorSuccess: "#7FBC39",
   colorError: "#F56C6C",
   textDefaultColor: '#252323',
   textDefaultFontSize: '14px',
+  backgroundDefaultColor: '#fff',
 };
