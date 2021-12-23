38c14d1801fcd41843279b1327879b674d40bd4b
luolong05 <luolong05@163.com>
2021-12-23 14:29:27 +0800

    add theme for baseComponents

---
+++ src/styles/baseComponents/theme.ts
@@ -0,0 +1,14 @@
+export default {
+  fontSizeSm: '12px',
+  fontSizeMd: '14px',
+  fontSizeLg: '16px',
+  buttonPaddingSm: '4px 10px',
+  buttonPaddingMd: '6px 15px',
+  buttonPaddingLg: '8px 18px',
+  inputPaddingSm: '4px 2px',
+  inputPaddingMd: '6px 4px',
+  inputPaddingLg: '8px 6px',
+  formFieldBorderColorDisabled: '#ddd',
+  formFieldBgColorDisabled: '#e8e8e8',
+  formFieldFontColorDisabled: '#bdbdbd',
+};
