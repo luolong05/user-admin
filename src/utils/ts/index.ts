362ab285693bff5b59bc41193839b000ecec3f00
luolong05 <luolong05@163.com>
2021-12-24 11:22:11 +0800

    add method getListLabelByValue

--- src/utils/ts/index.ts
+++ src/utils/ts/index.ts
@@ -1,2 +1,13 @@
 export const getValueByKey = <T, K extends keyof T>(obj: T, key: K): T[K] =>
   obj[key];
+
+export const getListLabelByValue = <T, K extends keyof T>(list: T[], value: T[K], labelKey?: K, valueKey?: K): T[K] => {
+  const lk = labelKey || 'label' as K;
+  const vk = valueKey || 'value' as K;
+  const item: T | undefined = list.find((item: T) => item[vk] === value);
+  if (item) {
+    return item[lk];
+  }
+
+  return value;
+}
