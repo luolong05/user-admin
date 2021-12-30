3862036a7a29885068e9e9a56846f9431bfe90ec
luolong05 <luolong05@163.com>
2021-12-30 14:41:28 +0800

    Rename optionType name;

--- src/components/modules/userForm/testData.ts
+++ src/components/modules/userForm/testData.ts
@@ -1,11 +1,7 @@
-import {OptionType} from "@commonUI/select";
+import {OptionType as SelectOptionType} from "@commonUI/select";
+import {OptionType as RadioOptionType} from "@commonUI/radio/radioGroup";
 
-interface UserActive {
-  value: number;
-  label: string;
-}
-
-export const userAreaList: OptionType[] = [
+export const userAreaList: SelectOptionType[] = [
   { value: "area1", label: 'Area-1' },
   { value: "area2", label: 'Area-2' },
   { value: "area3", label: 'Area-3' },
@@ -13,7 +9,7 @@ export const userAreaList: OptionType[] = [
   { value: "area5", label: 'Area-5' },
 ];
 
-export const userSkillList: OptionType[] = [
+export const userSkillList: SelectOptionType[] = [
   { value: "js", label: 'Javascript' },
   { value: "ts", label: 'Typescript' },
   { value: "html", label: 'Html' },
@@ -21,7 +17,7 @@ export const userSkillList: OptionType[] = [
   { value: "react", label: 'React' },
 ];
 
-export const userActiveList: UserActive[] = [
+export const userActiveList: RadioOptionType[] = [
   { value: 1, label: 'Yes' },
   { value: 0, label: 'No' },
 ];
