78cc8d2e0fb4df6acaf61f4b53d77dc7e2ef4b84
luolong05 <luolong05@163.com>
2022-01-04 10:53:30 +0800

    add deviceSize

--- src/config/layout/index.ts
+++ src/config/layout/index.ts
@@ -1,3 +1,11 @@
-export default {
-  mobileScreenMaxWidth: 768
+const deviceSize = {
+  mobileSm: '320px',
+  mobileMd: '375px',
+  mobileLg: '425px',
+  pad: '768px',
+  pcSm: '1366px'
+};
+
+export {
+  deviceSize
 }
